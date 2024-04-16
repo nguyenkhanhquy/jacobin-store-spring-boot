0e48b69540f430e26e78281f7897e9bcac413f64
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-16 23:29:11 +0700

    init: jacobin-store-spring-boot

---
+++ src/main/java/live/jacobin/JacobinStoreApplication.java
@@ -0,0 +1,13 @@
+package live.jacobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacobinStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacobinStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/jacobin/JacobinStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package live.jacobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobinStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
