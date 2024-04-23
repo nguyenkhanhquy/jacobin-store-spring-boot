680deb94e6a4534fbe82098cebe02ccbbcc23afd
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-23 17:39:49 +0700

    feat: delete category

--- src/main/java/live/jacobin/controller/admin/ManagerCategoryController.java
+++ src/main/java/live/jacobin/controller/admin/ManagerCategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,8 +29,6 @@ public class ManagerCategoryController {
         // Đặt danh sách vào model để truyền tới view
         model.addAttribute("ListC", listC);
 
-        model.addAttribute("ListC", listC);
-
         return "admin/manager_category_page";
     }
 
@@ -39,13 +38,14 @@ public class ManagerCategoryController {
     }
 
     @PostMapping("/add-category")
-    public String addCategory(@RequestParam String name, Model model) {
+    public String addCategory(@RequestParam String name,
+                              RedirectAttributes redirectAttributes) {
         String message;
         String messageError;
         if (categoryService.checkNameExists(name)) {
             messageError = "Tên danh mục đã tồn tại! Vui lòng điền tên khác.";
-            message = "";
-            model.addAttribute("name", name);
+            redirectAttributes.addFlashAttribute("messageError", messageError);
+            redirectAttributes.addFlashAttribute("name", name);
         }
         else {
             Category category = Category.builder()
@@ -55,15 +55,31 @@ public class ManagerCategoryController {
             categoryService.saveCategory(category);
 
             int idC = categoryService.selectCategoryByName(name).getCategoryId();
-
             message = "Thêm thành công danh mục [" + name + "] có mã [" + idC + "]";
-            messageError = "";
+            redirectAttributes.addFlashAttribute("message", message);
         }
 
-        model.addAttribute("message", message);
-        model.addAttribute("messageError", messageError);
+        return "redirect:/dashboard/manager-category/add-category";
+    }
 
-        return "admin/add_category_page";
+    @PostMapping("/delete")
+    public String deleteCategory(@RequestParam int categoryId,
+                                 @RequestParam String name,
+                                 RedirectAttributes redirectAttributes) {
+        String message;
+        String messageError;
+        Category category = categoryService.selectCategoryById(categoryId);
+        try {
+            categoryService.deleteCategory(category);
+            message = "Xoá thành công danh mục [" + categoryId + " - " + name + "]";
+            redirectAttributes.addFlashAttribute("message", message);
+        }
+        catch (Exception e) {
+            messageError = "Trong danh [" + categoryId + " - " + name + "] vẫn còn sản phẩm, hãy xoá sản phẩm trước!";
+            redirectAttributes.addFlashAttribute("messageError", messageError);
+        }
+
+        return "redirect:/dashboard/manager-category";
     }
 
 }
