c0732a4bb5c94432fcf70a25fe89b2d5adfff472
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-05-03 00:07:57 +0700

    feat: lock account staff

--- src/main/java/live/jacobin/controller/admin/ManagerStaffController.java
+++ src/main/java/live/jacobin/controller/admin/ManagerStaffController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -30,5 +33,25 @@ public class ManagerStaffController {
         model.addAttribute("ListU", listU);
 
         return "admin/staff/manager_staff_page";
+    }
+
+    @PostMapping("/lock")
+    public String lockCustomer(@RequestParam int userId,
+                               @RequestParam String email,
+                               RedirectAttributes redirectAttributes) {
+        String message;
+        String messageError;
+        User user = userService.selectUserById(userId);
+        try {
+            user.setLocked(true);
+            userService.saveUser(user);
+            message = "Khóa thành công tài khoản của nhân viên [" + userId + " - " + email + "]";
+            redirectAttributes.addFlashAttribute("message", message);
+        } catch (Exception e) {
+            messageError = "Khóa tài khoản của nhân viên [" + userId + " - " + email + "] không thành công";
+            redirectAttributes.addFlashAttribute("messageError", messageError);
+        }
+
+        return "redirect:/dashboard/manager-staff";
     }
 }
